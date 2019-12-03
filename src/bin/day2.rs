c9a6321e3d6391daff8028d1de7688899a14b227
LFalch <lucas@wasd.dk>
2019-12-03 08:11:48 +0100

    Do day 2 part one

---
+++ src/bin/day2.rs
@@ -0,0 +1,45 @@
+use std::io::{Result, BufRead, BufReader};
+use std::fs::File;
+
+type U = usize;
+
+const ADD: U = 1;
+const MUL: U = 2;
+const HALT: U = 99;
+
+fn read_file(p: &str) -> Result<Vec<U>> {
+    let mut v = Vec::with_capacity(32);
+
+    for bytes in BufReader::new(File::open(p)?).split(b',') {
+        let n = String::from_utf8(bytes?).unwrap().parse().unwrap();
+
+        v.push(n);
+    }
+
+    Ok(v)
+}
+
+fn work(vals: &mut [U]) {
+    let mut pointer = 0;
+    loop {
+        match vals[pointer] {
+            ADD => vals[vals[pointer+3]] = vals[vals[pointer+1]] + vals[vals[pointer+2]],
+            MUL => vals[vals[pointer+3]] = vals[vals[pointer+1]] * vals[vals[pointer+2]],
+            HALT => break,
+            _ => unimplemented!()
+        }
+        pointer += 4;
+    }
+}
+
+
+fn main() {
+    let mut v = read_file("day2.input").unwrap();
+    v[1] = 12;
+    v[2] = 2;
+
+    work(&mut v);
+
+    println!("{:?}", v);
+}
+
